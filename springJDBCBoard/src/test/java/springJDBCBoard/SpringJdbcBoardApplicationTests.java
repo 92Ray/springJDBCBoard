f5a8f6779f9207e76314dd037a46e1e742a4dc8e
Ray <dnl1992@naver.com>
2026-01-28 11:55:08 +0900

    [생성] springJDBCBoard 게시판기능 구현

---
+++ springJDBCBoard/src/test/java/springJDBCBoard/SpringJdbcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package springJDBCBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
